5d2fc492cdad2fe96eb49ec4f445f7bb9c621bec
maniaWEE <121031296+maniaWEE@users.noreply.github.com>
2026-03-03 21:17:08 +0800

    Add files via upload

---
+++ 9:15pm,3march/Parasite_Class.h
@@ -0,0 +1,32 @@
+#ifndef PARASITE_CLASS_H_INCLUDED
+#define PARASITE_CLASS_H_INCLUDED
+
+#include <array>
+#include <vector>
+
+class Parasite{
+    int num_of_parasites {5}; //initial number of parasite is 5
+    std::vector<std::array<double,2>> coords_of_parasites {};
+
+public:
+    //functions related to num_of_parasites
+    int getNum(); //get num_of_parasites
+    void increaseNum(); //increase num_of_parasites by 1
+
+    //functions related to coords_of_parasites
+    void print_Coords_list(); //print out coords of each parasite, for debugging
+    std::array<double,2> getCoords_of(size_t index); //get coords for a specific parasite in {x,y} format
+
+    void generateCoords_Draw(FILE* plot); //generate new coordinates and drawings
+
+    int deduct_Health(int immunity_level, int surv_current_hp);
+    /*pass thru getImmunity and getHp (whatever u call it into the argument)
+    returns deducted health for that survivor
+    so use it like:
+    newhp = parasite.deduct_Health(survivor.getImmunity, survivor.getHp);
+    survivor.setHp(newhp);
+    or you can combine it into one line, ur choice
+    */
+};
+
+#endif // PARASITE_CLASS_H_INCLUDED
